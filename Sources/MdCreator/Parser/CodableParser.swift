579c9e16e9e570b16120f7e25f1e50dadc62331a
Gleb-Kovalenko <yourpendos@gmail.com>
2022-08-03 19:59:47 +0300

    update documentation and refactor code

--- Sources/MdCreator/Parser/CodableParser.swift
+++ Sources/MdCreator/Parser/CodableParser.swift
@@ -12,7 +12,24 @@ import Foundation
 protocol CodableParser {
     
     /// Gets all properties from a file data with their values
-    /// - Parameter fileDate: File data to get all properties from
+    ///
+    /// Example:
+    ///
+    /// struct SomeStrcut: Codable {
+    ///     var firstProterty: String = "hello"
+    ///     var secondProperty: Character = "!"
+    ///     var thirdProperty: Int = 3
+    /// }
+    /// let resultFunction: [String: Any] = allProperties(from: SomeStruct)
+    ///
+    /// And 'resultFunction' has the form:
+    ///     [
+    ///       "firstProperty": "hello",
+    ///       "secondProperty": "!",
+    ///       "thirdProperty": 3
+    ///     ]
+    ///
+    /// - Parameter fileData: File data to get all properties from
     /// - Returns: Dictionary with file's properties and values
     func allProperties(from fileData: Codable) -> [String: Any]
     
@@ -20,15 +37,27 @@ protocol CodableParser {
     ///
     /// The parameters are specified in the form ${parameterName} or ${parameterName.someFunction}
     /// and the parameter name must consist only of letters
+    ///
+    /// Example:
+    ///
+    /// let someData: [String: Any] = [
+    ///           "firstProperty": "hello",
+    ///           "secondProperty": [
+    ///             "hello": "world",
+    ///             "someText": "some text ${firstParameter} some text",
+    ///             "someNumber": 5
+    ///           ],
+    ///           "thirdProperty": "some text ${secondParameter} some text"
+    ///     ]
+    /// let parametersFromSomeData = requiredParameters(from: someData)
+    ///
+    /// And 'parametersFromSomeData' has the form:
+    ///     [
+    ///       "firstParameter": "",
+    ///       "secondParameter": ""
+    ///     ]
+    /// 
     /// - Parameter data: Data from which you need to get all the necessary parameters
     /// - Returns: Dictionary with the names of the required parameters. Values are set as an empty string
     func requiredParameters(from data: [String: Any]) -> [String: String]
-    
-    /// Find the required parameters in the string
-    ///
-    /// The parameters are specified in the form ${parameterName} or ${parameterName.someFunction}
-    /// and the parameter name must consist only of letters
-    /// - Parameter string: String from which you need to get all the necessary parameters
-    /// - Returns: Array with the names of the required parameters.
-    func requiredParameters(from string: String) -> [String]
 }
--- Sources/MdCreator/Parser/CodableParserImplementation.swift
+++ Sources/MdCreator/Parser/CodableParserImplementation.swift
@@ -21,21 +21,21 @@ extension CodableParserImplementation: CodableParser {
         var allPropertiesDict: [String: Any] = [:]
         let mirror = Mirror(reflecting: fileData)
         for (mirrorProperty, mirrorValue) in mirror.children {
-            guard let property = mirrorProperty else {
+            guard let propertyMirror = mirrorProperty else {
                 continue
             }
-            allPropertiesDict[property] = mirrorValue
+            allPropertiesDict[propertyMirror] = mirrorValue
         }
         return allPropertiesDict
     }
     
     func requiredParameters(from data: [String: Any]) -> [String: String] {
         var requiredParametersDict: [String: String] = [:]
-        let dataValues = data.map { $0.value }
+        let dataValues = data.map ( \.value )
         for dataValue in dataValues {
             if let stringArrayElement = dataValue as? [String] {
                 let elementsWithParament = stringArrayElement.filter { $0.contains("${") }
-                if elementsWithParament.count > 0 {
+                if !elementsWithParament.isEmpty {
                     for elementWithParameter in elementsWithParament {
                         let parametersFromElement = requiredParameters(from: elementWithParameter)
                         for parameter in parametersFromElement {
@@ -58,7 +58,23 @@ extension CodableParserImplementation: CodableParser {
         return requiredParametersDict
     }
     
-    func requiredParameters(from string: String) -> [String] {
+    // MARK: - Private
+    
+    /// Find the required parameters in the string
+    ///
+    /// The parameters are specified in the form ${parameterName} or ${parameterName.someFunction}
+    /// and the parameter name must consist only of letters
+    ///
+    /// Example:
+    ///
+    /// let someString = "some text ${firstParameter}, some text ${secondParameter} some text"
+    /// let parametersFromSomeString = requiredParameters(from: someString)
+    ///
+    /// And 'parametersFromSomeString' has the form: ["firstParameter", "secondParameter"]
+    ///
+    /// - Parameter string: String from which you need to get all the necessary parameters
+    /// - Returns: Array with the names of the required parameters.
+    private func requiredParameters(from string: String) -> [String] {
         var elementWithParameter = string
         var allParameters: [String] = []
         var parameterName = ""
